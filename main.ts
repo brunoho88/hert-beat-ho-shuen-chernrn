7d7c245dca787235044e37dab7712a633e57fb09
brunoho88 <114266743+brunoho88@users.noreply.github.com>
2022-09-24 07:39:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        . . . . .
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . . . . .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - - - - ", 120)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G - G - G ", 120)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("- - - G G G G G ", 120)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("G G G G G G G G ", 120)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    music.playMelody("C C5 C C5 C - - - ", 120)
+})
